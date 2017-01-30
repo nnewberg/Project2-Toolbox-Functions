8ee728815f2bba0a2900a46f8daa8eefbdce0974
Nick Newberg <nick@spaces.com>
2017-01-30 15:13:56 -0500

    basic wing created

--- src/main.js
+++ src/main.js
@@ -4,6 +4,42 @@
 const THREE = require('three'); // older modules are imported like this. You shouldn't have to worry about this much
 import Framework from './framework'
 
+
+function easeLinear(t, start, end, duration){
+    return end * (t/duration) + start;
+}
+
+function ease_in_quadratic(t, start, end, duration){
+    t = t/duration;
+    t = t * t;
+    return t * (end - start) + start
+}
+
+function create_top_layer(wing, mesh, featherDist, numFeathers, steepness){
+     for (var i = 0; i < featherDist*numFeathers; i+=featherDist){
+        var featherMeshClone = mesh.clone();
+        var duration = featherDist*numFeathers;
+        var z_pos = ease_in_quadratic(i, 0, 1, duration);
+        featherMeshClone.position.set(i, 0, -z_pos);
+        var rot = easeLinear(i, 0.0, -90.0, duration);
+        featherMeshClone.rotateY((Math.PI/180.0) *rot)
+        wing.add(featherMeshClone);  
+    }
+}
+
+function create_bottom_layer(wing, mesh, featherDist, offset, numFeathers, steepness){
+     for (var i = 0; i < featherDist*numFeathers; i+=featherDist){
+        var featherMeshClone = mesh.clone();
+        var duration = featherDist*numFeathers;
+        var z_pos = ease_in_quadratic(i, 0, 1, duration);
+        featherMeshClone.position.set(i, -0.1, -z_pos + offset);
+        featherMeshClone.scale.set(1.5,1,2);
+        var rot = easeLinear(i, 0.0, -90.0, duration);
+        featherMeshClone.rotateY((Math.PI/180.0) *rot)
+        wing.add(featherMeshClone);  
+    }
+}
+
 // called after the scene loads
 function onLoad(framework) {
     var scene = framework.scene;
@@ -33,17 +69,33 @@ function onLoad(framework) {
 
     scene.background = skymap;
 
+    //parent wing object
+    var wing = new THREE.Object3D();
+    wing.name = "wing";
     // load a simple obj mesh
     var objLoader = new THREE.OBJLoader();
     objLoader.load('/geo/feather.obj', function(obj) {
 
         // LOOK: This function runs after the obj has finished loading
-        var featherGeo = obj.children[0].geometry;
 
+        var featherGeo = obj.children[0].geometry;
         var featherMesh = new THREE.Mesh(featherGeo, lambertWhite);
+        featherMesh.rotation.y = Math.PI / 2;
+        featherMesh.rotation.z = Math.PI;
+
+        wing.add(featherMesh);
         featherMesh.name = "feather";
-        scene.add(featherMesh);
+        var featherDist = 0.25;
+        var numFeathers = 20.0;
+        var steepness = 15.0; //lower is steeper
+        create_top_layer(wing, featherMesh, featherDist, numFeathers, steepness);
+        featherDist = 0.35;
+        numFeathers = 15.0;
+        steepness = 12.0; //lower is steeper
+        create_bottom_layer(wing, featherMesh, featherDist, 0, numFeathers, steepness);
+
     });
+    scene.add(wing);
 
     // set camera position
     camera.position.set(0, 1, 5);
@@ -61,11 +113,11 @@ function onLoad(framework) {
 
 // called on frame updates
 function onUpdate(framework) {
-    var feather = framework.scene.getObjectByName("feather");    
-    if (feather !== undefined) {
+    var wing = framework.scene.getObjectByName("wing");    
+    if (wing !== undefined) {
         // Simply flap wing
         var date = new Date();
-        feather.rotateZ(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);        
+        wing.rotateX(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);        
     }
 }
 
